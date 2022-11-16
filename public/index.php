098bb5c7d6e608342d12595fa200ab5408d098a0
talgat065 <talgat06585@gmail.com>
2022-11-16 21:12:57 +0600

    configure di container

--- public/index.php
+++ public/index.php
@@ -1,8 +1,32 @@
-<?php
+<?php declare(strict_types=1);
 
-require_once "../vendor/autoload.php";
+use App\Application\Dto\DrawPrizeRequest;
+use App\Application\Handlers\PrizeService;
+use App\Application\Repositories\ItemRepository;
+use App\Application\Repositories\PrizeRepository;
+use App\Application\Repositories\UserRepository;
+use App\Infrastructure\Repositories\SqlItemRepository;
+use App\Infrastructure\Repositories\SqlPrizeRepository;
+use App\Infrastructure\Repositories\SqlUserRepository;
 
-use App\User;
+require_once __DIR__ . "./../vendor/autoload.php";
 
-$u = new User('nother');
-echo $u->getName();
+$containerBuilder = new \DI\ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+
+$containerBuilder->addDefinitions([
+    ItemRepository::class => \DI\create(SqlItemRepository::class),
+    PrizeRepository::class => \DI\create(SqlPrizeRepository::class),
+    UserRepository::class => \DI\create(SqlUserRepository::class),
+    PrizeService::class => \DI\create(PrizeService::class)->constructor(
+        \Di\get(PrizeRepository::class),
+        \Di\get(UserRepository::class),
+        \Di\get(ItemRepository::class),
+    ),
+]);
+
+$container = $containerBuilder->build();
+
+$helloWorld = $container->get(PrizeService::class);
+$helloWorld->draw(new DrawPrizeRequest('uuid'));
